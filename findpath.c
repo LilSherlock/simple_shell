6f86b8d5719915025c9e19e5793dd67f6d6921a7
Lilsherlock <potatogames15@gmail.com>
2020-04-15 00:55:16 +0000

    path was create

---
+++ findpath.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+char *pathCat(char *dir, char *av)
+{
+	int i = 0, k = 0, length = 0, length2 = 0;
+	char *buf;
+
+	length = _stlren(dir);
+	length2 = _stlren(av);
+	buf = malloc(sizeof(char) * length + length2 + 1);
+	while (i < length)
+	{
+		buf[i] = dir[i];
+		i++;
+	}
+	while (i < length + length2)
+	{
+		buf[i] = av[k];
+		i++;
+		k++;
+	}
+	buf[i] = '\0';
+	return (buf);
+}
